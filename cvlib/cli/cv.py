2c48f4107d7a91db9f7865354ba88be5f69ff000
raffaello baluyot <baluyotraf@outlook.com>
2022-12-17 21:19:51 +0100

    Add theme configuration, reorganize the templates and config

--- cvlib/cli/cv.py
+++ cvlib/cli/cv.py
@@ -5,24 +5,25 @@ import click
 from cvlib import template
 
 
-def _html(input, output, web):
+DEF_CONFIG = 'cv.yaml'
+
+
+def _html(config, output, web):
     os.makedirs(output, exist_ok=True)
-
-    template.render_html(input, os.path.join(output, 'index.html'))
-    template.render_css(os.path.join(output, 'cv.css'), web=web)
+    template.render_page(config, output, 1 if web else 0)
 
 
 @click.command()
-@click.argument('input', type=click.Path(exists=True), default='cv.yaml')
+@click.argument('config', type=click.Path(exists=True), default=DEF_CONFIG)
 @click.argument('output', type=click.Path(), default='site')
-def html(input, output):
-    _html(input, output, True)
+def html(config, output):
+    _html(config, output, True)
 
 
 @click.command()
-@click.argument('input', type=click.Path(exists=True), default='cv.yaml')
+@click.argument('config', type=click.Path(exists=True), default=DEF_CONFIG)
 @click.argument('output', type=click.Path(), default='print')
-def print(input, output):
+def print(config, output):
     # Should be replaced by PDF option once there's a light weight library
     # that supports HTML5/CSS3
-    _html(input, output, False)
+    _html(config, output, False)

97c6fda9eac3fb0c8cecc3b19024b7ecd9cd88a6
raffaello baluyot <baluyotraf@outlook.com>
2022-12-19 00:24:26 +0100

    Update metadata of the html pages

--- cvlib/template.py
+++ cvlib/template.py
@@ -47,7 +47,7 @@ def render_page(config, output_dir, web):
 
     html_path = os.path.join(output_dir, HTML_OUTPUT_NAME)
     render_template(html_path, HTML_TEMPLATE_NAME, cv=cv,
-                    card_path=f'./{CARD_OUTPUT_NAME}')
+                    card_path=CARD_OUTPUT_NAME)
 
     css_path = os.path.join(output_dir, CSS_OUTPUT_NAME)
     render_template(css_path, CSS_TEMPLATE_NAME, cv=cv, web_mode=web)
